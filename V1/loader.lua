7a3baf8076e649a9db3362e555b4e437e5c361b1
Chip Hill <tncnfeardiscord@gmail.com>
2025-10-06 22:28:02 -0500

    🛠️ V1.3

--- V1/loader.lua
+++ V1/loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://raw.githubusercontent.com/StealADexter/StealADexter/main/V1/main.lua"))()
+loadstring(game:HttpGet("http://raw.githubusercontent.com/StealADexter/StealADexter/main/V1/main.lua"))() 
